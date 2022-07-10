df3adf5300611243f1c9d8b35b38b3efa1fca646
ROSH <95622188+roshnirajak@users.noreply.github.com>
2022-07-10 18:23:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+let turn = "X";
+
+//function to change turns
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X";
+}
+
+//function to check winner
+const checkWin = () => {
+
+}
+
+//game logic 
+
+
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.text');
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === "") {
+            boxtext.innerText = turn;
+            changeTurn();
+            document.getElementById("info")[0].innerText = turn;
+        }
+    })
+})
